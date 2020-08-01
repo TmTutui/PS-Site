f2e43ad03e1034ae6d39fa0a25bae23d5b919db8
Tulio <39662335+TmTutui@users.noreply.github.com>
2020-08-01 04:38:35 -0300

    FInish
    -> conditions on limite e taxes on score page
    -> integrate ML model to dinamic scores
    -> add databases

--- app.py
+++ app.py
@@ -1,29 +1,38 @@
 from flask import Flask, render_template, request
+
+from ML.FINAL_DA_WEB import ml_model
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/score', methods=['GET', 'POST'])
+@app.route('/score', methods=['GET'])
 def score():
   default_value = "1"
 
-  canal = int(request.form.get('canal_de_vendas_ord', default_value ))
-  subcanal = int(request.form.get('subcanal_de_vendas_ord', default_value ))
-  segmento = int(request.form.get('segmento_ord', default_value ))
-  regiao = int(request.form.get('regiao_ord', default_value ))
-  cidade = int(request.form.get('cidade_ord', default_value ))
-  bairro = int(request.form.get('bairro_ord', default_value ))
-  uf = int(request.form.get('uf_ord', default_value ))
-  mcc = int(request.form.get('mcc_ord', default_value ))
-  mei = int(request.form.get('mei', default_value ))
-  tpv = int(request.form.get('canal_de_vendas_ord', "5763" ))
+  canal = int(request.args.get('canal_de_vendas_ord', default_value ))
+  subcanal = int(request.args.get('subcanal_de_vendas_ord', default_value ))
+  segmento = int(request.args.get('segmento_ord', default_value ))
+  regiao = int(request.args.get('regiao_ord', default_value ))
+  cidade = int(request.args.get('cidade_ord', default_value ))
+  bairro = int(request.args.get('bairro_ord', default_value ))
+  uf = int(request.args.get('uf_ord', default_value ))
+  mcc = int(request.args.get('mcc_ord', default_value ))
+  mei = int(request.args.get('mei', default_value ))
+  tpv = int(request.args.get('tpv', "5763" ))
 
-  score = .8
-  limite = 10000
-  rotativo = "5%"
-  parcelas = "4%"
+  inputs = [tpv,segmento,mcc,bairro,cidade,regiao,uf,subcanal]
+
+  print(inputs)
+
+  limite, parcelas, score = ml_model(inputs)
+
+  score = score.round(2)
+  print(score)
+  limite = limite.round(0)
+  rotativo = str((1.5*parcelas).round(2)) + "%"
+  parcelas = str(parcelas.round(2)) + "%"
 
   if(mei == 1):
     # se for MEI
